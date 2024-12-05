5b469a4264713feed9267b0cfa8cc55c251c5927
Oskari Kosonen <o.kosonen@gmail.com>
2024-12-05 20:46:44 +0200

    Day 5 tasks

---
+++ src/test/test_day05.py
@@ -0,0 +1,63 @@
+"""Advent of Code 2024 - Unit tests for day 5 tasks"""
+
+from src.util import get_lines
+from ..day05 import part_one, part_two
+
+
+example_data = """47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
+""".split(
+    "\n"
+)
+
+
+def test_part_one() -> None:
+    """
+    Test function for part_one.
+
+    This function tests the implementation of the part_one function by asserting the expected output
+
+    Returns:
+        None
+    """
+    assert part_one(example_data) == 143
+    assert part_one(get_lines("day05")) == 5129
+
+
+def test_part_two() -> None:
+    """
+    Test function for part_two.
+
+    This function tests the implementation of the part_two function by asserting the expected output
+
+    Returns:
+        None
+    """
+    assert part_two(example_data) == 123
+    assert part_two(get_lines("day05")) == 4077
